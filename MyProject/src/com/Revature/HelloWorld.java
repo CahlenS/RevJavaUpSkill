a6fc19e2cec054cfc43e3f4d225b118e1d86e647
CahlenS <81649666+CahlenS@users.noreply.github.com>
2021-12-20 15:57:51 -0500

    Add files via upload

---
+++ MyProject/src/com/Revature/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.Revature;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+		Employee emp;
+		emp = new Employee();
+
+	}
+
+}
